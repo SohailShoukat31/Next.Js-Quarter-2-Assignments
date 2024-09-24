823609cf02a03763c64f4637918a859062e5c506
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-09-24 17:42:47 +0500

    👉Update🙂

---
+++ 01_component/component/card.tsx
@@ -0,0 +1,9 @@
+const Card = ()=>{
+    return(
+        <div>
+            <h4>This is Header section</h4>
+            <h4>This is About section</h4>
+        </div>
+    )
+}
+export default Card
---
+++ 01_component/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import Card from '../../component/card';
+export default function Home() {
+  return (
+   <div>
+    <h1>This is my second Assingment </h1>
+  
+   <Card/>
+    </div>
+  );
+}
